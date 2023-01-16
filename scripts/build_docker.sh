90eb8a60c1e2bdc5557212a4cb5bd30fc3ade48a
szahlner <stefan.zahlner@gmx.at>
2023-01-16 13:50:58 +0100

    update dockerfiles, use pytorch=1.11.0, cuda=11.3

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -2,22 +2,22 @@
 # Mostly taken from stablebaselines3 at: https://github.com/DLR-RM/stable-baselines3/blob/master/scripts/build_docker.sh
 
 CPU_PARENT=ubuntu:20.04
-GPU_PARENT=nvidia/cuda:11.4.0-base-ubuntu20.04
+GPU_PARENT=nvidia/cuda:11.3.0-runtime-ubuntu20.04
 
 TAG=szahlner/lcer
 VERSION=$(cat ./lcer/version.txt)
 
 if [[ ${USE_GPU} == "True" ]]; then
     PARENT=${GPU_PARENT}
-    PYTORCH_DEPS="cudatoolkit=11.3"
+    PYTORCH_INSTALL="pip install --no-input torch==1.11.0+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
 else
     PARENT=${CPU_PARENT}
-    PYTORCH_DEPS="cpuonly"
+    PYTORCH_INSTALL="pip install --no-input torch==1.11.0+cpu --extra-index-url https://download.pytorch.org/whl/cpu"
     TAG="${TAG}-cpu"
 fi
 
-echo "docker build --build-arg PARENT_IMAGE=${PARENT} --build-arg PYTORCH_DEPS=${PYTORCH_DEPS} -t ${TAG}:${VERSION} . -f ./Docker/Dockerfile"
-docker build --build-arg PARENT_IMAGE=${PARENT} --build-arg PYTORCH_DEPS=${PYTORCH_DEPS} -t ${TAG}:${VERSION} . -f ./Docker/Dockerfile
+echo "docker build --build-arg PARENT_IMAGE=${PARENT} --build-arg PYTORCH_INSTALL=${PYTORCH_INSTALL} -t ${TAG}:${VERSION} . -f ./Docker/Dockerfile"
+docker build --build-arg PARENT_IMAGE=${PARENT} --build-arg PYTORCH_INSTALL=${PYTORCH_INSTALL} -t ${TAG}:${VERSION} . -f ./Docker/Dockerfile
 docker tag ${TAG}:${VERSION} ${TAG}:latest
 
 if [[ ${RELEASE} == "True" ]]; then
