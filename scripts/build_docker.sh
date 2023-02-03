7a082e2f35431034ce9886dca2b80910eb41a5b7
szahlner <stefan.zahlner@gmx.at>
2023-02-03 13:08:02 +0100

    update docker build script runtime -> devel and faiss install path

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -2,7 +2,7 @@
 # Mostly taken from stablebaselines3 at: https://github.com/DLR-RM/stable-baselines3/blob/master/scripts/build_docker.sh
 
 CPU_PARENT=ubuntu:20.04
-GPU_PARENT=nvidia/cuda:11.3.0-runtime-ubuntu20.04
+GPU_PARENT=nvidia/cuda:11.3.0-devel-ubuntu20.04
 
 TAG=szahlner/lcer
 VERSION=$(cat ./lcer/version.txt)
@@ -10,14 +10,16 @@ VERSION=$(cat ./lcer/version.txt)
 if [[ ${USE_GPU} == "True" ]]; then
     PARENT=${GPU_PARENT}
     PYTORCH_INSTALL="pip install --no-input torch==1.11.0+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
+    FAISS_INSTALL="conda install -c pytorch faiss-gpu cudatoolkit=11.3"
 else
     PARENT=${CPU_PARENT}
     PYTORCH_INSTALL="pip install --no-input torch==1.11.0+cpu --extra-index-url https://download.pytorch.org/whl/cpu"
+    FAISS_INSTALL="conda install -c pytorch faiss-cpu"
     TAG="${TAG}-cpu"
 fi
 
-echo "docker build --build-arg PARENT_IMAGE=${PARENT} --build-arg PYTORCH_INSTALL=${PYTORCH_INSTALL} -t ${TAG}:${VERSION} . -f ./Docker/Dockerfile"
-docker build --build-arg PARENT_IMAGE=${PARENT} --build-arg PYTORCH_INSTALL=${PYTORCH_INSTALL} -t ${TAG}:${VERSION} . -f ./Docker/Dockerfile
+echo "docker build --build-arg PARENT_IMAGE=${PARENT} --build-arg PYTORCH_INSTALL=${PYTORCH_INSTALL} --build-arg FAISS_INSTALL=${FAISS_INSTALL} -t ${TAG}:${VERSION} . -f ./Docker/Dockerfile"
+docker build --build-arg PARENT_IMAGE=${PARENT} --build-arg PYTORCH_INSTALL=${PYTORCH_INSTALL} --build-arg FAISS_INSTALL=${FAISS_INSTALL} -t ${TAG}:${VERSION} . -f ./Docker/Dockerfile
 docker tag ${TAG}:${VERSION} ${TAG}:latest
 
 if [[ ${RELEASE} == "True" ]]; then
